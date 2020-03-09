544ea2ea01a01fec342ebba47840ca144a89c315
lsmok1 <lsmok225@gmail.com>
2020-03-09 09:38:37 -0400

    fixed bug with win conditions and implemented play again feature

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,7 +5,8 @@ public class TicTacToe {
     static ArrayList<Integer> cpuPositions = new ArrayList<>();
     public static void main (String[] args) {
         //initializing the gameboard
-        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+        char[][] gameBoard = {
+                {' ', '|', ' ', '|', ' '},
                 {'-', '+', ' ', '+', '-'},
                 {' ', '|', ' ', '|', ' '},
                 {'-', '+', ' ', '+', '-'},
@@ -18,39 +19,42 @@ public class TicTacToe {
         char user = scan.nextLine().charAt(0);
         char cpu = ' ';
         if (user == 'x') {
-            cpu = 'y';
+            cpu = 'o';
         } else {
             cpu = 'x';
         }
         // while (true) loops code indefinitely
         while (true) {
             scan = new Scanner(System.in);
+
             System.out.println("Your move! (1-9): ");
             int pos = scan.nextInt();
             //checking if the pos you entered is already in the array list or
             // if you are trying to take a position taken by the computer already
-            while(playerPositions.contains(pos) || cpuPositions.contains(playerPositions)) {
+            while(playerPositions.contains(pos) || cpuPositions.contains(pos)) {
                 System.out.println("Position is already taken... Enter another position: ");
                 pos = scan.nextInt();
             }
             //placepiece by taking in the initial char[][], getting the int pos from scanner, and getting the char user from scanner
-            placePiece(gameBoard, pos, user);
+            placePiece(gameBoard, pos, "user", user);
             //checking the result after you place a piece
             String result = winnerWinnerChickenDinner();
             //checking if there even is a result after every time you place a piece
             if (result.length()>0) {
                 System.out.println(result);
-                break;
+                playAgain(user, gameBoard);
             }
 
 
             Random rand = new Random();
             int cpuPos = rand.nextInt(9) + 1;
-            placePiece(gameBoard, cpuPos, cpu);
+
             //checking if the number the computer generated is already taken by you or if the computer already generated that position
             while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
                 cpuPos = rand.nextInt(9) + 1;
             }
+            //cpu places the piece after the while loop check
+            placePiece(gameBoard, cpuPos, "cpu", cpu);
 
             printGameBoard(gameBoard);
             //checking result after cpu places a piece
@@ -58,8 +62,9 @@ public class TicTacToe {
             //checking if there is a result or just an empty string
             if (result.length()>0) {
                 System.out.println(result);
-                break;
+                playAgain(user, gameBoard);
             }
+
         }
     }
     public static void printGameBoard(char[][] gameBoard){
@@ -73,15 +78,11 @@ public class TicTacToe {
             System.out.println();
         }
     }
-    public static void placePiece(char[][] gameBoard, int pos, char user) {
+    public static void placePiece(char[][] gameBoard, int pos, String user, char playerSymbol) {
         //initializing the char
-        char symbol = ' ';
+        char symbol = Character.toUpperCase(playerSymbol);
         //checking if the person chose x or not
-        if(user == 'x') {
-            symbol = 'X';
-            playerPositions.add(pos);
-        } else if (user == 'o') {
-            symbol = 'O';
+        if(user.equals("user")) {
             playerPositions.add(pos);
         } else {
             cpuPositions.add(pos);
@@ -90,7 +91,8 @@ public class TicTacToe {
             case 1:
                 gameBoard[0][0] = symbol;
                 break;
-            case 2: // [0][1] is the space for the line '|'
+                // [0][1] is the space for the line '|'
+            case 2:
                 gameBoard[0][2] = symbol;
                 break;
             case 3:
@@ -142,12 +144,37 @@ public class TicTacToe {
         for (List l : winCon) {
             if (playerPositions.containsAll(l)) {
                 return "You won!";
+
             } else if (cpuPositions.containsAll(l)) {
                 return "A computer that randomly generates numbers won...";
-            } else if (playerPositions.size() + cpuPositions.size() == 9){
-                return "Tie";
             }
         }
+        //took this out of the for loop
+        if (playerPositions.size() + cpuPositions.size() == 9){
+            return "Tie";
+        }
         return "";
+    }
+    public static void playAgain(char user, char [][] gameBoard) {
+        System.out.println("Would you like to play again? (Y/N): ");
+        Scanner play = new Scanner(System.in);
+        char yOrN = play.nextLine().charAt(0);
+        if (yOrN == 'y') {
+            playerPositions.clear();
+            cpuPositions.clear();
+            gameBoard[0][0] = ' ';
+            gameBoard[0][2] = ' ';
+            gameBoard[0][4] = ' ';
+            gameBoard[2][0] = ' ';
+            gameBoard[2][2] = ' ';
+            gameBoard[2][4] = ' ';
+            gameBoard[4][0] = ' ';
+            gameBoard[4][2] = ' ';
+            gameBoard[4][4] = ' ';
+            user = ' ';
+
+        } else {
+            return;
+        }
     }
 }
